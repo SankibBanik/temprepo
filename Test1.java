7b27b8c2d07d1c8d93e493ec96fc0a3d598f296f
Sankib Banik <sankibbanik2464@gmail.com>
2024-08-02 12:47:33 +0530

    From local repo

---
+++ Test1.java
@@ -0,0 +1,8 @@
+public class Test1{
+	public void display(){
+		System.out.println("Hello");
+	}
+	public int addNumbers(int x,int y){
+		return x+y;
+	}
+} 
---
+++ TestMain.java
@@ -0,0 +1,6 @@
+public class TestMain{
+	public static void main(String[] args){
+		Test1 t = new Test1();
+		t.display();
+	}
+}
